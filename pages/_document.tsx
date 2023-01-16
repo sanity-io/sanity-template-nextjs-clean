35089e3b5f3c443f497ff91ef25950ddcc75aa54
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2023-01-16 15:13:34 +0100

    fix: dark bg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-white text-black">
+      <body className="bg-white text-black dark:bg-black dark:text-white">
         <Main />
         <NextScript />
       </body>
