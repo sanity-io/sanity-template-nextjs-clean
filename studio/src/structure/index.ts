ee1c39f3aed3a7f06d89147fdeec00d8d5ffdcb7
KenJonesPizza <kenjwho@gmail.com>
2024-12-18 12:12:56 -0600

    Update structure

--- studio/src/structure/index.ts
+++ studio/src/structure/index.ts
@@ -7,23 +7,6 @@ import type {StructureBuilder, StructureResolver} from 'sanity/structure'
  * Learn more: https://www.sanity.io/docs/structure-builder-introduction
  */
 
-// export const structure: StructureResolver = (S: any) =>
-//   S.list()
-//     .title('Website Content')
-//     .items([
-//       S.documentTypeListItem('post').title('Posts'),
-//       S.documentTypeListItem('page').title('Pages'),
-//       S.documentTypeListItem('person').title('People'),
-//       // Settings Singleton in order to view/edit the one particular document for Settings.  Learn more about Singletons: https://www.sanity.io/docs/create-a-link-to-a-single-edit-page-in-your-main-document-type-list
-//       S.listItem()
-//         .title('Site Settings')
-//         .child(S.document().schemaType('settings').documentId('siteSettings'))
-//         .icon(CogIcon),
-//     ])
-
-// ./deskStructure.js
-// import {CogIcon} from '@sanity/icons'
-
 const DISABLED_TYPES = ['settings', 'assist.instruction.context']
 
 export const structure: StructureResolver = (S: StructureBuilder) =>
@@ -34,6 +17,7 @@ export const structure: StructureResolver = (S: StructureBuilder) =>
         // Remove the "assist.instruction.context" and "settings" content  from the list of content types
         (listItem: any) => !DISABLED_TYPES.includes(listItem.getId()),
       ),
+      // Settings Singleton in order to view/edit the one particular document for Settings.  Learn more about Singletons: https://www.sanity.io/docs/create-a-link-to-a-single-edit-page-in-your-main-document-type-list
       S.listItem()
         .title('Site Settings')
         .child(S.document().schemaType('settings').documentId('siteSettings'))
