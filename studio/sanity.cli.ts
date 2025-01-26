0f93d70c51834c4e2c954cb2f860ae143ef3c76d
KenJonesPizza <kenjwho@gmail.com>
2025-01-26 12:56:54 -0600

    Update dependencies.

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -15,6 +15,6 @@ export default defineCliConfig({
     projectId,
     dataset,
   },
-  studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to leanr more about using environment variables for local & production.
+  studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to learn more about using environment variables for local & production.
   autoUpdates: true,
 })
