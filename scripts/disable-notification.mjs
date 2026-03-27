9a9d97e0de25234df170b445cd25e7dbcdb0d332
KenJonesPizza <kenjwho@gmail.com>
2026-03-27 11:00:40 -0500

    Add restart reminder to disable-notification script

--- scripts/disable-notification.mjs
+++ scripts/disable-notification.mjs
@@ -10,6 +10,7 @@ try {
   }
   writeFileSync(filePath, JSON.stringify(data, null, 2) + '\n')
   console.log('Notification disabled in sanity-template.json')
+  console.log('Restart the dev server for this change to take effect.')
 } catch (err) {
   console.error('Could not disable notification:', err.message)
   process.exit(1)
