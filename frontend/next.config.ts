a8fc570a24dc46b9cd2dba117d1a05d96e80f44d
Cody Olsen <stipsan@gmail.com>
2026-06-01 21:20:05 +0200

    feat: use next typegen, bump deps

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,6 @@
 import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
-  env: {
-    // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
-    SC_DISABLE_SPEEDY: 'false',
-  },
   images: {
     remotePatterns: [new URL('https://cdn.sanity.io/**')],
   },
